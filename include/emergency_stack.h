6af2a1c148f1f9a21c2b1e0bf95e02e4c6dd6930
Amaya-Senadheera <amayasenadheera6@gmail.com>
2026-03-21 14:22:26 +0530

    Update emergency_stack.h

---
+++ include/emergency_stack.h
@@ -0,0 +1,18 @@
+#ifndef EMERGENCY_STACK_H
+#define EMERGENCY_STACK_H
+
+// Function declarations (prototypes)
+
+// Add emergency patient (Push)
+void add_emergency_patient();
+
+// Treat patient (Pop)
+void treat_patient();
+
+// Display emergency patients
+void display();
+
+// Run full emergency system (menu)
+void run_emergency_system();
+
+#endif
